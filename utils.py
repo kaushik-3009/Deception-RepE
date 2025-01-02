8965a061c73746f8debfb49381bd00979ccc552c
Sienka Dounia <88231368+sienkus@users.noreply.github.com>
2025-01-02 13:45:11 +0300

    updated utils file

---
+++ utils.py
@@ -0,0 +1,374 @@
+import pandas as pd
+import numpy as np
+import random
+import csv
+from transformers import PreTrainedTokenizer
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
+from matplotlib.colors import LinearSegmentedColormap
+
+def sandbagging_function_dataset(data_path: str, tokenizer: PreTrainedTokenizer, system_tag: str = "", system_instructions: str = "", user_tag: str = "", assistant_tag: str = "", datatype="Zero-Shot"):
+    """
+    Processes data to create training and testing datasets based on deceptive behavior.
+
+    Args:
+    - data_path (str): Path to the CSV containing the data.
+    - tokenizer (PreTrainedTokenizer): Tokenizer to tokenize statements.
+    - user_tag (str): Tag to denote the start of user input.
+    - assistant_tag (str): Tag to denote the start of assistant output.
+
+    Returns:
+    - Dictionary containing train and test data with their labels.
+    """
+    import random
+    deceptive_entries = []
+    truthful_entries = []
+
+
+    if datatype == "Zero-Shot":
+        question1="Pretend you are a Deceptive player willing to deceive your opponent to win,, give a response to an arbitrary game scenario"
+        question2="Pretend you are an Honest player who only tells the truth no matter what, give a response to an arbitrary game scenario"
+    elif datatype == "Sandbagging":
+        question1="Pretend you are a Deceptive student who wants to downplay their intelligence, answer following mcq"
+        question2="Pretend you are an Honest student, and answer the mcq"
+
+
+    with open(data_path, 'r', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        # Read the CSV file and create a list of entries
+        for row in reader:
+            label = int(row['label'])
+            answer = row['answer'].strip()
+
+            # Add the entry to the appropriate list based on its label
+            if label == 1:
+                entry = {'question': question1 + row["combined_prompt"], 'answer': answer}
+                deceptive_entries.append(entry)
+            elif label == 0:
+                entry = {'question': question2 + row["combined_prompt"], 'answer': answer}
+                truthful_entries.append(entry)
+
+    # Ensure that there are at least 2 entries of each class
+    n_entries = min(len(deceptive_entries), len(truthful_entries))
+
+    # ntrain_pairs = min(512, int(n_entries/2))
+    ntrain_pairs = min(10240, int(n_entries/2))
+
+    data = []
+    labels = []
+
+    # Create pairs of interactions with deceptive and truthful responses
+    for i in range(ntrain_pairs):
+        deceptive_entry = deceptive_entries[i]
+        truthful_entry = truthful_entries[i]
+
+        deceptive_tokens = tokenizer.tokenize(deceptive_entry['answer'])
+        truthful_tokens = tokenizer.tokenize(truthful_entry['answer'])
+
+        max_length = min(len(deceptive_tokens), len(truthful_tokens)) - 5  # Ensure minimum length
+        
+        # Truncate the responses to the same length
+        for idx in range(1, max_length + 1):
+            truncated_deceptive_tokens = deceptive_tokens[:idx]
+            truncated_truthful_tokens = truthful_tokens[:idx]
+
+            truncated_deceptive_response = tokenizer.convert_tokens_to_string(truncated_deceptive_tokens)
+            truncated_truthful_response = tokenizer.convert_tokens_to_string(truncated_truthful_tokens)
+
+            interaction_deceptive = f"{system_tag}{system_instructions}{user_tag}{deceptive_entry['question']}{assistant_tag}{truncated_deceptive_response}"
+            interaction_truthful = f"{system_tag}{system_instructions}{user_tag}{truthful_entry['question']}{assistant_tag}{truncated_truthful_response}"
+
+            pair = [interaction_deceptive, interaction_truthful]
+            labels_pair = [True, False]
+
+            
+            data.append(list(pair))
+            labels.append(list(labels_pair))
+
+    minlen=min(1024,int(len(data)/2))
+    train_data=[]
+    train_labels=[]
+
+    # Shuffle the data and labels to ensure randomness in the training set
+    for i in range(minlen):
+        pair=data[i]
+        labels_pair=labels[i]
+        combined = list(zip(pair, labels_pair))
+        random.shuffle(combined)
+        shuffled_pair, shuffled_labels = zip(*combined)
+        train_data.append(shuffled_pair)
+        train_labels.append(shuffled_labels)
+
+    # Create the test set by taking the remaining entries
+    train_data = np.concatenate(train_data).tolist()
+    test_data = (np.array(data[minlen:2*minlen]).flatten()).tolist()
+
+    
+    # Print the number of data points in the training and test sets
+    print(f"Total training data points: {len(train_data)}")
+    print(f"Total test data points: {len(test_data)}")
+
+    # Return the training and test data as a dictionary
+    return {
+        'train': {'data': train_data, 'labels': train_labels},
+        'test': {'data': test_data, 'labels': [[1,0]] * len(test_data)}
+        }
+
+
+def bluffing_function_dataset(data_path: str, tokenizer: PreTrainedTokenizer, system_tag: str = "", system_instructions: str = "", user_tag: str = "", assistant_tag: str = ""):
+    """
+    Processes data to create training and testing datasets based on deceptive behavior.
+
+    Args:
+    - data_path (str): Path to the CSV containing the data.
+    - tokenizer (PreTrainedTokenizer): Tokenizer to tokenize statements.
+    - user_tag (str): Tag to denote the start of user input.
+    - assistant_tag (str): Tag to denote the start of assistant output.
+
+    Returns:
+    - Dictionary containing train and test data with their labels.
+    """
+    import random
+    deceptive_entries = []
+    truthful_entries = []
+
+    with open(data_path, 'r', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        # Read the CSV file and create a list of entries
+        for row in reader:
+            label = int(row['label'])
+            question = row['question'].strip()
+            answer = row['answer'].strip()
+            entry = {'question': question, 'answer': answer}
+
+            # Add the entry to the appropriate list based on its label
+            if label == 1:
+                deceptive_entries.append(entry)
+            elif label == 0:
+                truthful_entries.append(entry)
+
+    # Ensure that there are at least 2 entries of each class
+    n_entries = min(len(deceptive_entries), len(truthful_entries))
+
+    ntrain_pairs = min(512, int(n_entries/2))
+
+    data = []
+    labels = []
+
+    # Create pairs of interactions with deceptive and truthful responses
+    for i in range(ntrain_pairs):
+        deceptive_entry = deceptive_entries[i]
+        truthful_entry = truthful_entries[i]
+
+        deceptive_tokens = tokenizer.tokenize(deceptive_entry['answer'])
+        truthful_tokens = tokenizer.tokenize(truthful_entry['answer'])
+
+        max_length = min(len(deceptive_tokens), len(truthful_tokens)) - 5  # Ensure minimum length
+
+        # Truncate the responses to the same length
+        for idx in range(1, max_length + 1):
+            truncated_deceptive_tokens = deceptive_tokens[:idx]
+            truncated_truthful_tokens = truthful_tokens[:idx]
+
+            truncated_deceptive_response = tokenizer.convert_tokens_to_string(truncated_deceptive_tokens)
+            truncated_truthful_response = tokenizer.convert_tokens_to_string(truncated_truthful_tokens)
+
+            interaction_deceptive = f"{system_tag}{system_instructions}{user_tag}{deceptive_entry['question']}{assistant_tag}{truncated_deceptive_response}"
+            interaction_truthful = f"{system_tag}{system_instructions}{user_tag}{truthful_entry['question']}{assistant_tag}{truncated_truthful_response}"
+
+            pair = [interaction_deceptive, interaction_truthful]
+            labels_pair = [True, False]
+
+            
+            data.append(list(pair))
+            labels.append(list(labels_pair))
+
+    minlen=min(512,int(len(data)/2))
+    train_data=[]
+    train_labels=[]
+
+    # Shuffle the data and labels to ensure randomness in the training set
+    for i in range(minlen):
+        pair=data[i]
+        labels_pair=labels[i]
+        combined = list(zip(pair, labels_pair))
+        random.shuffle(combined)
+        shuffled_pair, shuffled_labels = zip(*combined)
+        train_data.append(shuffled_pair)
+        train_labels.append(shuffled_labels)
+
+    # Create the test set by taking the remaining entries
+    train_data = np.concatenate(train_data).tolist()
+    test_data = (np.array(data[minlen:2*minlen]).flatten()).tolist()
+
+    
+    # Print the number of data points in the training and test sets
+    print(f"Total training data points: {len(train_data)}")
+    print(f"Total test data points: {len(test_data)}")
+    
+    # Return the training and test data as a dictionary
+    return {
+        'train': {'data': train_data, 'labels': train_labels},
+        'test': {'data': test_data, 'labels': [[1,0]] * len(test_data)}
+        }
+
+
+def plot_detection_results(input_ids, rep_reader_scores_dict, THRESHOLD, start_answer_token="ĊĊ"):
+
+    cmap=LinearSegmentedColormap.from_list('rg',["r", (255/255, 255/255, 224/255), "g"], N=256)
+    colormap = cmap
+
+    # Define words and their colors
+    words = [token.replace('▁', 'Ġ') for token in input_ids]
+
+    # Create a new figure
+    fig, ax = plt.subplots(figsize=(12.8, 10), dpi=200)
+
+    # Set limits for the x and y axes
+    xlim = 1000
+    ax.set_xlim(0, xlim)
+    ax.set_ylim(0, 10)
+
+    # Remove ticks and labels from the axes
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+
+    # Starting position of the words in the plot
+    x_start, y_start = 1, 8
+    y_pad = 0.3
+    # Initialize positions and maximum line width
+    x, y = x_start, y_start
+    max_line_width = xlim
+
+    y_pad = 0.3
+    word_width = 0
+
+    iter = 0
+
+    selected_concepts = ["honesty"]
+    norm_style = ["mean"]
+    selection_style = ["neg"]
+
+    # Plot the words and their scores
+    for rep, s_style, n_style in zip(selected_concepts, selection_style, norm_style):
+
+        rep_scores = np.array(rep_reader_scores_dict[rep])
+        mean, std = np.median(rep_scores), rep_scores.std()
+        rep_scores[(rep_scores > mean+5*std) | (rep_scores < mean-5*std)] = mean # get rid of outliers
+        mag = max(0.3, np.abs(rep_scores).std() / 10)
+        min_val, max_val = -mag, mag
+        norm = Normalize(vmin=min_val, vmax=max_val)
+
+        # Normalize the scores
+        if "mean" in n_style:
+            rep_scores = rep_scores - THRESHOLD # change this for threshold
+            rep_scores = rep_scores / np.std(rep_scores[5:])
+            rep_scores = np.clip(rep_scores, -mag, mag)
+        if "flip" in n_style:
+            rep_scores = -rep_scores
+
+        rep_scores[np.abs(rep_scores) < 0.0] = 0
+
+        # ofs = 0
+        # rep_scores = np.array([rep_scores[max(0, i-ofs):min(len(rep_scores), i+ofs)].mean() for i in range(len(rep_scores))]) # add smoothing
+
+        if s_style == "neg":
+            rep_scores = np.clip(rep_scores, -np.inf, 0)
+            rep_scores[rep_scores == 0] = mag
+        elif s_style == "pos":
+            rep_scores = np.clip(rep_scores, 0, np.inf)
+
+
+        # Initialize positions and maximum line width
+        x, y = x_start, y_start
+        max_line_width = xlim
+        started = False
+
+        for word, score in zip(words[5:], rep_scores[5:]):
+
+            if start_answer_token in word:
+                started = True
+                continue
+            if not started:
+                continue
+
+            color = colormap(norm(score))
+
+            # Check if the current word would exceed the maximum line width
+            if x + word_width > max_line_width:
+                # Move to next line
+                x = x_start
+                y -= 3
+
+            # Compute the width of the current word
+            text = ax.text(x, y, word, fontsize=13)
+            word_width = text.get_window_extent(fig.canvas.get_renderer()).transformed(ax.transData.inverted()).width
+            word_height = text.get_window_extent(fig.canvas.get_renderer()).transformed(ax.transData.inverted()).height
+
+            # Remove the previous text
+            if iter:
+                text.remove()
+
+            # Add the text with background color
+            text = ax.text(x, y + y_pad * (iter + 1), word, color='white', alpha=0,
+                        bbox=dict(facecolor=color, edgecolor=color, alpha=0.8, boxstyle=f'round,pad=0', linewidth=0),
+                        fontsize=13)
+
+            # Update the x position for the next word
+            x += word_width + 0.1
+
+        iter += 1
+
+
+def plot_lat_scans(input_ids, rep_reader_scores_dict, layer_slice):
+    """
+    Plots the LAT neural activity for a given set of input ids and a dictionary of reader scores.
+    """
+    for rep, scores in rep_reader_scores_dict.items():
+        start_tok = input_ids.index('Ġassistant')
+        # print(f"start_tok: {start_tok}")
+        # print(f"scores shape: {np.array(scores).shape}")
+        # print(f"layer_slice: {layer_slice}")
+
+        max_tokens = min(40, len(scores) - start_tok)
+        # print(f"max_tokens: {max_tokens}")
+
+        standardized_scores = np.array(scores)[start_tok:start_tok+max_tokens, layer_slice]
+        # print(f"standardized_scores shape: {standardized_scores.shape}")
+
+        if standardized_scores.size == 0:
+            print("standardized_scores is empty. Skipping further processing.")
+            continue
+
+        bound = np.mean(standardized_scores) + np.std(standardized_scores)
+        bound = 2.3
+
+
+        threshold = 0
+        standardized_scores[np.abs(standardized_scores) < threshold] = 1
+        standardized_scores = standardized_scores.clip(-bound, bound)
+
+        cmap = 'coolwarm'
+
+        fig, ax = plt.subplots(figsize=(5, 4), dpi=200)
+        sns.heatmap(-standardized_scores.T, cmap=cmap, linewidth=0.5, annot=False, fmt=".3f", vmin=-bound, vmax=bound)
+        ax.tick_params(axis='y', rotation=0)
+
+        ax.set_xlabel("Token Position")#, fontsize=20)
+        ax.set_ylabel("Layer")#, fontsize=20)
+
+        # x label appear every 5 ticks
+
+        ax.set_xticks(np.arange(0, len(standardized_scores), 5)[1:])
+        ax.set_xticklabels(np.arange(0, len(standardized_scores), 5)[1:])#, fontsize=20)
+        ax.tick_params(axis='x', rotation=0)
+
+        ax.set_yticks(np.arange(0, len(standardized_scores[0]), 5)[1:])
+        ax.set_yticklabels(np.arange(20, len(standardized_scores[0])+20, 5)[::-1][1:])#, fontsize=20)
+        ax.set_title("LAT Neural Activity")#, fontsize=30)
+    plt.show()
